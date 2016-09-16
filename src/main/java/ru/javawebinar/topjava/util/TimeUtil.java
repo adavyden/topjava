ffb69e13afa6d84eb78cab042056a91a10feccb8
Anton <anton.d@mail.ru>
2016-09-16 11:36:16 +0400

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -8,6 +8,7 @@ import java.time.LocalTime;
  */
 public class TimeUtil {
     public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        int delete = 5;
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
 }
